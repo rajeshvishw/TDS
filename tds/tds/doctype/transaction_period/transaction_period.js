61bae6bdfac7506f3fdb03905dde6f3b976e70fa
rajeshvishw <rajeshvishwakarma7280@gmail.com>
2026-02-08 12:24:50 +0530

    First Commit

---
+++ tds/tds/doctype/transaction_period/transaction_period.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Rajesh Vishwakarma and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Transaction Period", {
+// 	refresh(frm) {
+
+// 	},
+// });
